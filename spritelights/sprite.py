79a3f22b915dbfd3dbadb5a7b44ee4c173bc93d0
Anstedt <anstedt@gmail.com>
2023-03-08 18:54:47 -0600

    Start of sprite

---
+++ spritelights/sprite.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from rpi_ws281x import Color
+
+Lemon       = Color(255,247,0)
+PaleYellow  = Color(254,255,162)
+SpringGreen = Color(0, 255,127)
+Mint        = Color(189,252,201)
+PalePink    = Color(255,192,203)
+LightSalmon = Color(255,160,122)
+Lavender    = Color(181,115,220)
+LightPlum   = Color(221,160,221)
+Lilac       = Color(200,162,200)
+Violet      = Color(238,130,238)
+SkyBlue     = Color(135,206,235)
+RobinEggBlue= Color(0,204,204)
+Turquoise   = Color(64,224,208)
