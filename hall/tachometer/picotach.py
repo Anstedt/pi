40e5737b3853412b702608b67e1b39ca8dfbc2d4
Howard J. Anstedt <anstedt@gmail.com>
2023-09-07 18:36:51 -0500

    Using CB functions works

--- hall/tachometer/picotach.py
+++ hall/tachometer/picotach.py
@@ -6,8 +6,22 @@ Hall_State = machine.Pin(25,machine.Pin.OUT) #use on board LED
 
 # Create an 'object' for our Hall Effect Sensor
 # When sensor is near magnet, signal is pulled to zero volts
-Hall_Input = machine.Pin(15,machine.Pin.IN,machine.Pin.PULL_UP)
- 
+Hall_Input = machine.Pin(22,machine.Pin.IN,machine.Pin.PULL_UP)
+
+def Hall_CB(pin):
+    if (pin.value() == 1):
+        print("CB Off")
+    elif (pin.value() == 0):
+        print("CB On")
+    else:
+        print("CB", pin)
+
+Hall_Input.irq(Hall_CB)
+
+
+# Pb_Switch = machine.Pin(22,machine.Pin.IN,machine.Pin.PULL_UP)
+
+
 print("Ready, Set, Go!")
 while True:                     # Run an endless loop - Typical main loop   
   if Hall_Input.value() == 1:   # Sensor is pulled high, no magnet present
