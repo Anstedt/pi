425d9fd9efb6efe18385f2ae176b96fdf8a981ea
Howard J. Anstedt <anstedt@gmail.com>
2023-10-02 19:37:51 -0500

    Adjust inner loop times as well handle outer loop overhead

--- tachometer/pico-sm-tach.py
+++ tachometer/pico-sm-tach.py
@@ -5,27 +5,23 @@ from time import sleep, sleep_ms, sleep_us, ticks_us
 import select
 import sys
 
-# Count pulses, send count to main and reset on HALL change,
-# (autopull=true, pull_thresh=16) Not sure if useful to me
+# Count pulses, send count to main and reset on HALL change
 @asm_pio(set_init=PIO.OUT_HIGH)
 def hall_sensor():
   # label("loop")
   pull(block) # Get count down value from main in osr
-  # in_(osr,32) # Shift 32 bits data from osr to isr
   mov(x, osr) # Save X to use as count down start point
 
   label("loop")
   mov(y, x)
-  # in_(y, 32)
-  # push()
 
-  # Wait for the magnet
+  # Wait for the magnet, add one extra nop this makes each Y(tick) = 3us
   label("high")
-  jmp(y_dec, "inchigh") # Jump if Y NOT zero before decrement
+  jmp(y_dec, "inchigh") [1] # Jump if Y NOT zero before decrement
   label("inchigh")
   jmp(pin, "high") # Wait for low, magnet sensed when low
 
-  # Wait till magnet gone
+  # Wait till magnet gone, already 3 us because of extra jump
   label("low")
   jmp(y_dec, "inclow") # Jump if Y NOT zero before decrement
   label("inclow")
@@ -47,22 +43,24 @@ class PicoTach:
   
   def calc(self):
     # Loop time high = 2 low = 3, not sure how long is jump to label
-    loop_ticks = 2 + 3 # How many ticks per loop
+    # each loop is 3 ticks or 3us, which means are our 
+    ticks3us = 3 # How many ticks per sm loop
     countstart = 1000000000
+    overhead = 4 # time in us of overhead for full "loop"
     self.sma.put(countstart)
     print("pushcnt tx_fifo get")
     try:
       while True:
         if (self.sma.rx_fifo()):
-          # ticks_us = 1us
-          # loop_ticks_us = ticks_us * loop_ticks
-          # ticks = counterstart - get
-          # So this tells us the number of us that have gone by
-          # us = (countstart - self.sma.get()) * loop_ticks
+          # each tick = 3us
+          # us = (counterstart - get) * ticks3us
+          # So if counterstart - get == 2 then 6us have gone by
+          # us = (countstart - self.sma.get()) * ticks3us
+          # us = us + overhead of loop, based on sm loop analysis
           # Convert to seconds
           # secs = us / 1000000
           # rpm = 1/(secs)*60
-          print((1/(((countstart - self.sma.get()) * loop_ticks) / 1000000))*60)
+          print((1/( (((countstart - self.sma.get()) * ticks3us) + overhead) / 1000000))*60)
         
     except KeyboardInterrupt:
       print("Good Bye")
