69ff26bdb2f20c55d2c7de88da9e677f34014796
Howard J. Anstedt <anstedt@gmail.com>
2023-10-03 16:05:52 -0500

    Removed special case in sm for underflow, handle it in main code

--- tachometer/pico-sm-tach.py
+++ tachometer/pico-sm-tach.py
@@ -32,21 +32,18 @@ def hall_sensor():
   # Wait for the magnet, add one extra nop this makes each Y(tick) = 3us
   label("high")
   jmp(y_dec, "inchigh") [1] # Jump if Y NOT zero before decrement
-  jmp("outzero") # NOT included in loop count since it only happens on underflow
+  jmp("out") # NOT included in loop count since it only happens on underflow
   label("inchigh")
   jmp(pin, "high") # Wait for low, magnet sensed when low
 
   # Wait till magnet gone, already 3 us because of extra jump
   label("low")
   jmp(y_dec, "inclow") # Jump if Y NOT zero before decrement
-  jmp("outzero") # NOT included in loop count since it only happens on underflow
+  jmp("out") # NOT included in loop count since it only happens on underflow
   label("inclow")
   jmp(pin, "out")
   jmp("low")
 
-  label("outzero") # If we hit 0, send the count back as an indicator we rolled over
-  mov(y, x)
-  
   # Done so send count down value to main
   label("out")
   in_(y, 32) # Save y to isr
@@ -65,23 +62,22 @@ class PicoTach:
     self.freq = 1000000
     self.sma = StateMachine(1, hall_sensor, freq=self.freq, jmp_pin=Pin(22, Pin.IN, Pin.PULL_UP))  # Instantiate SM1, GPIO22
     self.sma.active(1)
-    # sleep(1)
   
   def calc(self):
-    # each loop is 3 ticks or 3us, which means are our 
     instructions_per_loop = 3 # How many ticks per sm loop
     tick_countstart = int(30 * (self.freq/instructions_per_loop)) # 1000000000 # for 30 seconds to 0, 30 * (freq / instructions_per_loop)
     overhead = 4 # number of overhead instructions for full "loop"
     overhead_period = overhead / self.freq # How long per tick
     tick_period = instructions_per_loop / self.freq # How long per tick
     self.sma.put(int(tick_countstart))
-    print("pushcnt tx_fifo get")
-    print("tick_countstart=", tick_countstart)
+    print("Startup")
+    # print("tick_countstart=", tick_countstart)
     try:
       while True:
         if (self.sma.rx_fifo()):
           ticks_from_sm = self.sma.get()
-          if (ticks_from_sm != tick_countstart):
+          # This is the Y underflow value
+          if (ticks_from_sm != 4294967295):
             ticks = tick_countstart - ticks_from_sm # How many ticks
             period = (ticks * tick_period) + overhead_period # total time that has pasted
             rpm = (1/period) * 60 # time in seconds to minutes, 1/time_secs period to rate / second, * 60
